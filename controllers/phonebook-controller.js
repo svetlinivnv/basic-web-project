aa8a00aa128c40a4d3a22ee99980fc0e03e9171b
Svetlin Ivanov <svetlin1995@hotmail.com>
2024-03-29 15:09:29 +0200

    asdf

--- controllers/phonebook-controller.js
+++ controllers/phonebook-controller.js
@@ -1,19 +1,26 @@
 const phonebook = require('../phonebook');
-const Contact = require('../models/Contact');
+// const Contact = require('../models/Contact');
+
+class Contact {
+  constructor(name, number) {
+      this.name = name;
+      this.number = number;
+  }
+}
 
 module.exports = {
   index: (req, res) => {
     res.render('index', {
-    // TODO: load index page
     contacts: phonebook.getPhonebook()
   });
 },
   addPhonebookPost:(req, res) => {
-    // TODO: add a phonebook object to the array
     let name = req.body.name;
     let number = req.body.number;
     let contact = new Contact(name, number);
+    if (name != '' && number != '') {
     phonebook.addContact(contact);
+    }
     res.redirect('/');
   }
 }
