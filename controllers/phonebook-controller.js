d0702c4db50de95cfac1d171b30a890b913e388e
Svetlin Ivanov <svetlin1995@hotmail.com>
2024-03-29 14:34:53 +0200

    basic web

--- controllers/phonebook-controller.js
+++ controllers/phonebook-controller.js
@@ -1,11 +1,19 @@
 const phonebook = require('../phonebook');
+const Contact = require('../models/Contact');
 
 module.exports = {
   index: (req, res) => {
-    res.render('index');
+    res.render('index', {
     // TODO: load index page
-  },
+    contacts: phonebook.getPhonebook()
+  });
+},
   addPhonebookPost:(req, res) => {
     // TODO: add a phonebook object to the array
+    let name = req.body.name;
+    let number = req.body.number;
+    let contact = new Contact(name, number);
+    phonebook.addContact(contact);
+    res.redirect('/');
   }
 }
--- phonebook.js
+++ phonebook.js
@@ -3,3 +3,18 @@
 	add methods for adding in the phonebook and getting it
 	export the methods
 */
+
+let phonebook = [];
+
+function getPhonebook() {
+	return phonebook;
+}
+
+function addContact(contact) {
+	phonebook.push(contact);
+}
+
+module.exports = {
+	getPhonebook,
+	addContact
+};
